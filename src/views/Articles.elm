57fd87ed159c7bb700a913cb702f1651dd047bd7
Martin MENESTRET <martin.menestret@axa-groupsolutions.com>
2017-02-22 16:16:48 +0100

    Better update function and beggining of auto expanding article list

--- src/views/Articles.elm
+++ src/views/Articles.elm
@@ -60,7 +60,7 @@ nextArticle articles a =
 
 isAfterShortList : List Article -> Int -> Article -> Bool
 isAfterShortList articles shortListSize currentArticle =
- False
+ articles |> articlesDesc |> (List.take shortListSize) |> (List.member currentArticle)
 
 pager : Model -> Html Msg
 pager model =
